4b00401f5acc27262c8b9290aa6683b423517a2d
unknown <inthiyazbasha9876@gmail.com>
2020-09-28 12:51:53 +0530

    images are added

--- albums.js
+++ albums.js
@@ -2,40 +2,48 @@ var albumList = [
     {
         albumName:'NUVVOSTANANTE NENODDANTANA',
         albumId:"NVNV",
+        image:'./images/nvnv.jpg',
         albumReleased:2005
     },
     {
         albumName:'Kotta Bangaru Lokam',
         albumId:"KBL",
+        image:'./images/kbl.jpg',
         albumReleased:2008
     },
     {
         albumName:'Tagore',
         albumId:"TA",
+        image:'./images/ta.jpg',
         albumReleased:2003
     },
     {
         albumName:'Cheppave Chirugaali',
         albumId:"CC",
+        image:'./images/cc.jpg',
         albumReleased:2004
     },
     {
         albumName:'Shankar Dada M.B.B.S.',
         albumId:"SD MBBS",
+        image:'./images/sdmbbs.jpg',
         albumReleased:2004
     },{
         albumName:'Ala Vaikunthapurramuloo',
         albumId:"AVL",
+        image:'./images/avl.jpg',
         albumReleased:2020
     },
     {
         albumName:'Geetha Govindam',
         albumId:"GG",
+        image:'./images/gg.jpg',
         albumReleased:2018
     },
     {
         albumName:'Taxiwaala',
         albumId:"TXWL",
+        image:'./images/txw.jpg',
         albumReleased:2019
     }
 ]
--- index.js
+++ index.js
@@ -1,24 +1,73 @@
+let album = document.getElementById('album');
+let albumsList = getAlbums();
+let artist = document.getElementById('artist')
+let playlist = document.getElementById('playlist')
+let dataAppend = document.getElementById('data')
+
+let albumcount=0
+let artistcount=0
+let playlistcount=0
+
 getalbumSongs();
 
-function getalbumSongs() {
-    let album = document.getElementById('album')
+function getalbumSongs() { 
+  if(albumcount==0){
     album.classList.add('active')
     playlist.classList.remove('active')
-    artist.classList.remove('active')
+    artist.classList.remove('active');
+
+
+    for (let i of albumsList) {
+        let div = document.createElement('div')
+        div.id=i.albumId
+        let movieName = document.createElement('p')
+        let year = document.createElement('p')
+        let image=document.createElement('img')
+        image.setAttribute("src", i.image);
+        image.classList.add('image')
+        movieName.innerHTML = 'Movie : ' + i.albumName;
+        movieName.addEventListener('click', () => {
+            getdetials(i.albumId);
+        }, false)
+        year.innerHTML = 'Year : ' + i.albumReleased;
+        movieName.classList.add('albums')
+        div.appendChild(image)
+        div.appendChild(movieName)
+        div.appendChild(year)
+        dataAppend.appendChild(div)
+    }
+    albumcount++;
+  }else{
+      removeOldData()
+      albumcount=0;
+      getalbumSongs()
+  }
 }
 
 function getartistSongs() {
-    let artist = document.getElementById('artist')
     artist.classList.add('active')
     album.classList.remove('active')
     playlist.classList.remove('active')
+    removeOldData()
+    albumcount=0
 
 }
 
 function getplaylistSongs() {
-    let playlist = document.getElementById('playlist')
     playlist.classList.add('active')
     album.classList.remove('active')
     artist.classList.remove('active')
 
+}
+
+function getdetials(e) {
+    console.log(e);
+}
+
+
+function removeOldData(){
+    for(let i of albumsList){
+        let removeid=document.getElementById(i.albumId)
+        removeid.remove()
+    }
 }
